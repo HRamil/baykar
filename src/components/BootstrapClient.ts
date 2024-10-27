7f4660b4a512c0bfa1a5d3a6f69f625b9ce9da2c
HRamil <ramilhasanov2000@gmail.com>
2024-10-27 16:55:12 +0300

    son

--- src/components/BootstrapClient.ts
+++ src/components/BootstrapClient.ts
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 export default function BootstrapClient(): null {
 
     useEffect(() => {
-        // @ts-expect-error
+        // @ts-expect-error  test
         import('bootstrap/dist/js/bootstrap.bundle.min.js')
             .then(() => {
                 console.log("Bootstrap JavaScript özellikleri yüklendi");
