946ae95784a46eca36d71bc7bc4f8b6042f70a1f
HRamil <ramilhasanov2000@gmail.com>
2024-10-29 00:24:42 +0300

    best of best

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import BestOfBest from "@/components/bestOfBest";
 import SneakersPart from "@/components/sneakersPart";
 
 export default function Home() {
   return (
     <div>
       <SneakersPart />
+      <BestOfBest />
     </div>
   );
 }
---
+++ src/components/bestOfBest/index.tsx
@@ -0,0 +1,60 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+//style
+import './bestOfBest.scss'
+
+//style
+import best1 from '@/images/best1.svg'
+import best2 from '@/images/best2.svg'
+import best3 from '@/images/best3.svg'
+import shoping from '@/images/shopping-cart.svg'
+
+export default function BestOfBest() {
+
+    const arr = [
+        {
+            image: best1,
+            title: 'Title',
+            text: 'Egestas elit dui scelerisque ut eu purus aliquam vitae habitasse.',
+        },
+        {
+            image: best2,
+            title: 'Title',
+            text: 'Egestas elit dui scelerisque ut eu purus aliquam vitae habitasse.',
+        },
+        {
+            image: best3,
+            title: 'Title',
+            text: 'Egestas elit dui scelerisque ut eu purus aliquam vitae habitasse.',
+        }
+    ]
+
+    return (
+        <div className='bestOfBest'>
+            <div className="title d-flex justify-content-between align-items-center container-fluid">
+                <h2 className="mb-0">The best of the best</h2>
+                <Link href="#" className="btn border-white text-light d-flex justify-content-center align-items-center">Sign up now</Link>
+            </div>
+            <div className='bg'>
+                <div className="container-fluid d-flex justify-content-between">
+                    {arr.map((el: any) => {
+                        return (
+                            <div className="card" style={{ width: '27.4076vw' }}>
+                                <Image src={el.image} className="card-img-top" alt={el.title} />
+                                <div className="card-body">
+                                    <h5 className="card-title">{el.title}</h5>
+                                    <p className="card-text">{el.text}</p>
+                                    <Link href="#" className="btn d-flex justify-content-center align-items-center">
+                                        <Image src={shoping} alt='shoping' />
+                                        <p>Buy Now</p>
+                                    </Link>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
+    )
+}
