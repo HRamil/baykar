52b9cae0dc131ff9e9e86d394cf9a7120003bb71
HRamil <ramilhasanov2000@gmail.com>
2024-10-30 02:31:43 +0300

    sneaker mobile version

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,36 +2,37 @@ import React from 'react'
 
 //style
 import './header.scss'
+import Link from 'next/link'
 
 export default function Header() {
     return (
         <header>
             <nav className="navbar navbar-expand-lg">
-                <div className="container-fluid">
+                <div className="container-fluid d-flex justify-content-between">
                     <a className="navbar-brand" href="#">Navbar</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <ul className="navbar-nav mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Products</a>
+                                <Link className="nav-link active" aria-current="page" href="#">Products</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Solutions</a>
+                                <Link className="nav-link" href="#">Solutions</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Pricing</a>
+                                <Link className="nav-link" href="#">Pricing</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Resources</a>
+                                <Link className="nav-link" href="#">Resources</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Log In</a>
+                                <Link className="nav-link" href="#">Log In</Link>
                             </li>
                         </ul>
-                        <form className="d-flex" role="search">
-                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        <form className="d-flex" role="sign up">
+                            <button className="btn" type="submit">Sign up now</button>
                         </form>
                     </div>
                 </div>
