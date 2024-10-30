a458f45aa985c3c2aaf9380bbb27bfd728be4f7d
HRamil <ramilhasanov2000@gmail.com>
2024-10-30 07:00:02 +0300

    header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,22 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import './header.scss';
+import Image from 'next/image';
 
 //style
-import './header.scss'
-import Link from 'next/link'
+import menu from '@/images/menu-scale.svg'
 
 export default function Header() {
     return (
         <header>
             <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid d-flex justify-content-between">
-                    <a className="navbar-brand" href="#">Navbar</a>
+                    <a className="navbar-brand" href="#">Collers</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                            <Image src={menu} alt='menu' />
                     </button>
-                    <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
-                        <ul className="navbar-nav mb-2 mb-lg-0">
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" href="#">Products</Link>
+                                <Link className="nav-link" aria-current="page" href="#">Products</Link>
                             </li>
                             <li className="nav-item">
                                 <Link className="nav-link" href="#">Solutions</Link>
@@ -31,12 +33,12 @@ export default function Header() {
                                 <Link className="nav-link" href="#">Log In</Link>
                             </li>
                         </ul>
-                        <form className="d-flex" role="sign up">
+                        <form className="d-flex">
                             <button className="btn" type="submit">Sign up now</button>
                         </form>
                     </div>
                 </div>
             </nav>
         </header>
-    )
+    );
 }
