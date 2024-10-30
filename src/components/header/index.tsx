50fe9d6d19aa219990a60933f43f53d1ea12835f
HRamil <ramilhasanov2000@gmail.com>
2024-10-30 07:02:35 +0300

    header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -15,8 +15,8 @@ export default function Header() {
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <Image src={menu} alt='menu' />
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                        <ul className="navbar-nav  mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <Link className="nav-link" aria-current="page" href="#">Products</Link>
                             </li>
